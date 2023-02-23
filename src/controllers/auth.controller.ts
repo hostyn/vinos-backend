63fd48411b98c8ab347ac3f6b397fab3f93a96e1
Hostyn <ruben@martinezhostyn.com>
2023-02-23 19:11:27 +0100

    Add /login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,18 +4,41 @@ import { PRIVATE_KEY } from '../config'
 import User from '../models/User'
 
 export const loginHandler = async (req: Request, res: Response) => {
-  res.status(200).json({ status: 'ok' })
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  // If user not found
+  if (!user) {
+    res.status(404).json({ error: 'Wrong user or password' })
+    return
+  }
+
+  // If wrong password
+  if (!User.comparePassword(password, user.password)) {
+    res.status(404).json({ error: 'Wrong user or password' })
+    return
+  }
+  // We use the same response to prevent user enumeration
+
+  // Create jsonwebtoken
+  const token = jwt.sign({ id: user._id }, PRIVATE_KEY, {
+    expiresIn: 7 * 24 * 60 * 60, // 7 days
+  })
+
+  res.status(200).json({ token })
 }
 
 export const registerHandler = async (req: Request, res: Response) => {
   const { email, password } = req.body
 
-  // Create user, encrypt password and save to database
+  // Create user and encrypt password
   const newUser = new User({
     email,
     password: User.encryptPassword(password),
   })
 
+  // Save to database
   const savedUser = await newUser.save()
 
   // Create jsonwebtoken
