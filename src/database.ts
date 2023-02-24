9a9c9f6a03c3dd89858a9b9577ddbd3616cf4ad1
Hostyn <ruben@martinezhostyn.com>
2023-02-24 17:35:57 +0100

    Minor fiexes

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import { MONGODB_URI } from './config'
 
+mongoose.set('strictQuery', true)
+
 export const db = mongoose
   .connect(MONGODB_URI)
   .then(db => console.log('Database is connected to', db.connection.name))
