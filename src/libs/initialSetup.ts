4b621d0cacabe7a296b7b35c4acdd7d41e322850
Hostyn <ruben@martinezhostyn.com>
2023-02-26 18:32:58 +0100

    Change sample measurements

--- src/libs/initialSetup.ts
+++ src/libs/initialSetup.ts
@@ -34,33 +34,33 @@ export const createWineTypesAndWineVarieties = async (): Promise<void> => {
   if (storedMeasurements.length === 0) {
     await new WineMeasurement({
       year: 2012,
-      variety: wineVarieties[0]._id,
-      type: wineTypes[0]._id,
+      variety: wineVarieties[1]._id,
+      type: wineTypes[1]._id,
       alcohol: 5.5,
-      observations: '',
-      color: 'tinto',
+      observations: 'VEGA REAL EL EMPECINADO',
+      color: 'Tinto',
       ph: 3.3,
       temperature: 21.5,
     }).save()
 
     await new WineMeasurement({
-      year: 2019,
-      variety: wineVarieties[4]._id,
-      type: wineTypes[1]._id,
+      year: 2021,
+      variety: wineVarieties[5]._id,
+      type: wineTypes[0]._id,
       alcohol: 4.6,
-      observations: '',
-      color: 'blanco',
+      observations: 'ATRIUM',
+      color: 'Blanco',
       ph: 3,
       temperature: 23.2,
     }).save()
 
     await new WineMeasurement({
-      year: 2008,
+      year: 2016,
       variety: wineVarieties[2]._id,
-      type: wineTypes[3]._id,
+      type: wineTypes[2]._id,
       alcohol: 6.5,
-      observations: '',
-      color: 'tinto',
+      observations: 'CHIVITE',
+      color: 'Tinto',
       ph: 3.1,
       temperature: 20.1,
     }).save()
